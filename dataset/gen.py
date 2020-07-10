e7384c3e1b1420763407e52f9afcf09d87ad2c8c
pixelzerg <pixelzerg@gmail.com>
2020-07-10 15:48:51 +0100

    UI work

--- dataset/gen.py
+++ dataset/gen.py
@@ -7,16 +7,37 @@ from qtpy import QtWidgets, QtGui
 import json
 
 class ResultWidget(QtWidgets.QWidget):
-    def __init__(self, parent):
-        super().__init__(parent)
+    def __init__(self, item):
+        super().__init__()
+        self._item_data = {}
+
         self.layout = QtWidgets.QHBoxLayout()
         self.thumbnail = QtWidgets.QLabel()
+        self.title = QtWidgets.QLabel()
         self.layout.addWidget(self.thumbnail, 0)
-        self.layout.addLayout(self.textQVBoxLayout, 1)
+        self.layout.addWidget(self.title, 1)
         self.setLayout(self.layout)
 
-    def _set_thumbnail(self, image_path):
-        self.thumbnail.setPixmap(QtGui.QPixmap(image_path))
+    def _load_item(self, item):
+        item_id = item.get("id")
+        item_snippet = item.get("snippet")
+
+        # set id
+        if item_id.get("kind") != "youtube#video":
+            raise ValueError
+        self._item_data["id"] = item_id.get("videoId")
+
+        # load title
+        item_title = item_snippet.get("title")
+        self._item_data["title"] = item_title
+        self.title.setText(item_title)
+
+        # load image
+        thumbnail_url = item_snippet.get("thumbnails", {}).get("default", {}).get("url", None)
+        if thumbnail_url is not None:
+            pixmap = QtGui.QPixmap()
+            pixmap.loadFromData(requests.get(thumbnail_url).content)
+            self.thumbnail.setPixmap(pixmap)
 
 class ResultsSelector(QtWidgets.QMainWindow):
     def __init__(self, items):
@@ -25,6 +46,18 @@ class ResultsSelector(QtWidgets.QMainWindow):
         self.setFixedSize(300, 700)
 
         self.list_widget = QtWidgets.QListWidget(self)
+        for item in items:
+            try:
+                result_widget = ResultWidget(item)
+
+                list_item = QtWidgets.QListWidgetItem(self.list_widget)
+                list_item.setSizeHint(result_widget.sizeHint())
+
+                self.list_widget.addItem(list_item)
+                self.list_widget.setItemWidget(list_item, result_widget)
+            except ValueError:
+                pass
+
         self.setCentralWidget(self.list_widget)
 
 def youtube_api(endpoint, params):
@@ -69,7 +102,11 @@ try:
 except IndexError:
     print("Please provide a songs path")
 
-# app = QtWidgets.QApplication(sys.argv)
-# window = ResultsSelector()
-# window.show()
-# app.exec()
+# with open("out.json", "w") as f:
+#     json.dump(youtube_search("akane sasu"), f)
+
+with open("out.json", "r") as f:
+    app = QtWidgets.QApplication(sys.argv)
+    window = ResultsSelector(json.load(f))
+    window.show()
+    app.exec()
