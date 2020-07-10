4ef6facba3288eec48452822404b09b28365fa37
pixelzerg <pixelzerg@gmail.com>
2020-07-11 00:21:31 +0100

    begin work on grammar

--- dev.py
+++ dev.py
@@ -1,17 +1,27 @@
 import json
+import random
 from lark import Lark
+from pprint import pprint
 
 parser = Lark.open("title.lark", parser="earley")
 
 with open("dataset/out.json", "r") as f:
     dataset = json.load(f)
 
-results = []
-for key, song in dataset.items():
-    for result in song.get("results", []):
-        try:
-            results.append(result["title"])
-        except KeyError:
-            pass
+results = [
+    "50 cent - In Da Club (HQ)"
+]
 
-print(parser.parse("Hi mate"))
+if False:
+    for key, song in dataset.items():
+        for result in song.get("results", []):
+            try:
+                results.append(result["title"])
+            except KeyError:
+                pass
+
+random.shuffle(results)
+for result in results[:10]:
+    tree = parser.parse(result)
+    print("{:50.50}".format(result))
+    print(tree.pretty("\t"))
