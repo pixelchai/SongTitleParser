2c09715ba3fecd46a43b05d2e73a44279f35b65d
pixelzerg <pixelzerg@gmail.com>
2020-07-11 07:28:08 +0100

    grammar work: live meta

--- dev.py
+++ dev.py
@@ -1,18 +1,23 @@
 import json
 import random
 from lark import Lark
+import re
 from pprint import pprint
 
-parser = Lark.open("title.lark", parser="earley")
+parser = Lark.open("title.lark", parser="earley",  g_regex_flags=re.I)
 
 with open("dataset/out.json", "r") as f:
     dataset = json.load(f)
 
 results = [
-    "50 cent - In Da Club (HQ)"
+    "きて50 cent -- In Da Club (HQ) (HD)",
+    "The Killers - Mr Brightside (Live at Glastonbury, UK 2019)",
+    "The Killers - Mr Brightside (Live at Glastonbury, 2019, UK)",
+    "Florence + The Machine - Times Like These - Live At Glastonbury 2015",
+    "[Eng/Indo/Lyric] Aimyon - 『She Used to Be Alive, Right?』 Ikite Itanda Yo Na 生きていたんだよな"
 ]
 
-if False:
+if True:
     for key, song in dataset.items():
         for result in song.get("results", []):
             try:
@@ -20,8 +25,8 @@ if False:
             except KeyError:
                 pass
 
-random.shuffle(results)
+# random.shuffle(results)
 for result in results[:10]:
     tree = parser.parse(result)
-    print("{:50.50}".format(result))
+    print(result)
     print(tree.pretty("\t"))
